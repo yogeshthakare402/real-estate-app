d0f5e7f46bce7e20ba79efb8c2572c4e3af55b45
Vidhi Dhama <dhamavidhi12@gmail.com>
2022-11-23 16:31:09 +0530

    App Component updated

--- real-estate/client/src/App.js
+++ real-estate/client/src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter,Routes, Route} from 'react-router-dom'
+import BasicInfo from './components/addNewProperty/BasicInfo';
+import PropertyDetail from './components/addNewProperty/PropertyDetail';
+import GeneralInfo from './components/addNewProperty/GeneralInfo';
+import LocationInfo from './components/addNewProperty/LocationInfo';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Routes>
+        <Route path="basic_info" element={<BasicInfo />} />
+        <Route path="property_detail" element={<PropertyDetail />} />
+        <Route path="general_info" element={<GeneralInfo />} />
+        <Route path="location_info" element={<LocationInfo />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
