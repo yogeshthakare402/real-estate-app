546e568e4e45bb3d4756ab91186bf1965951ac8d
Vidhi Dhama <dhamavidhi12@gmail.com>
2022-11-25 22:59:10 +0530

    basic info form updated

--- real-estate/client/src/components/addNewProperty/BasicInfo.js
+++ real-estate/client/src/components/addNewProperty/BasicInfo.js
@@ -1,30 +1,27 @@
 import React, { useState } from 'react';
 import '../RealEstate.css'
 import {Link} from "react-router-dom";
-import PropertyNav from './PropertyNav';
 {/*********import view page  */}
 
-export default function BasicInfo() {
-  const [inputs, setInputs] = useState({});
-
-  const handleChange = (event) => {
-    setInputs(event.target.value)
-  }
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
+export default function BasicInfo({ nextStep, handleFormData, values }) {
+const submitFormData = (e) => {
+    e.preventDefault();
     
-  }
+      nextStep();
+   
+  };
+
 
   return (
     <>
-    <PropertyNav/>
+    
+    
     <div className='card'>
-    <form method='POST' action='#' onSubmit={handleSubmit}>
+    <form method='POST' action='#' onSubmit={submitFormData}>
         <section className='formSection'>
         <div className='formInput'>
             <label>Property Type</label>
-            <select value={inputs} onChange={handleChange}>
+            <select name="property" value={values.firstName} onChange={handleFormData("property")}>
                 <option>Select Property Type</option>
                 <option value="Plot">Plot</option>
                 <option value="House">House</option>
@@ -33,7 +30,7 @@ export default function BasicInfo() {
         </div>
         <div className='formInput'>
             <label>Negotable</label>
-            <select value={inputs} onChange={handleChange}>
+            <select name='negotable'>
                 <option value="#">Select Negotable</option>
             </select>
         </div>
@@ -43,25 +40,24 @@ export default function BasicInfo() {
                 type="text" 
                 name="price" 
                 placeholder='Example:10000'
-                value={inputs.price || ""} 
-                onChange={handleChange}
+                // value={inputs.price} 
             />
         </div>
         <div className='formInput'>
             <label>Ownership</label>
-            <select value={inputs} onChange={handleChange}>
+            <select name='ownership'>
                 <option value="#">Select Ownership</option>
             </select>
         </div>
         <div className='formInput'>
             <label>Property Age</label>
-            <select value={inputs} onChange={handleChange}>
+            <select name='property_age'>
                 <option value="#">Select Property Age</option>
             </select>
         </div>
         <div className='formInput'>
             <label>Property Approved</label>
-            <select value={inputs} onChange={handleChange}>
+            <select name='approved'>
                 <option value="#">Property Approved</option>
             </select>
         </div>
@@ -70,22 +66,27 @@ export default function BasicInfo() {
             <input 
             type="text" 
             name="description" 
-            value={inputs.description || ""} 
-            onChange={handleChange}
+            // value={inputs.description} 
+            // onChange={handleChange}
             />
         </div>
         <div className='formInput'>
             <label>Bank Loan</label>
-            <select value={inputs} onChange={handleChange}>
+            <select name='bank loan'>
                 <option value="#">Bank Loan</option>
         </select>
         </div>
         
         <div className='formInput'>
-            <button className='cancelBtn'>Cancel</button> {/************Add link for view page */}
+            <Link to={"/hello"}>
+                <button className='cancelBtn'>Cancel</button> {/************Add link for view page */}
+            </Link>
         </div>
+        
         <div className='formInput'>
-            <Link style={{textDecoration: 'none'}} to={"/property_detail"}><button className='saveBtn'>Save & Continue</button></Link>
+            
+                <button type='submit' className='saveBtn'>Save & Continue</button>
+            
             </div>
         </section>
         
