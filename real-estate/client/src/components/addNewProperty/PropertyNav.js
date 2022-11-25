21e9afefb6357f2f637ceb6df2c8e42b2343a982
Vidhi Dhama <dhamavidhi12@gmail.com>
2022-11-25 23:02:04 +0530

    navbar property updated

--- real-estate/client/src/components/addNewProperty/PropertyNav.js
+++ real-estate/client/src/components/addNewProperty/PropertyNav.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../RealEstate.css';
-// import {NavLink} from 'react-router-dom';
+
 import {
 Nav,
 NavLink,
@@ -9,34 +9,51 @@ Bars,
 NavMenu
 } from './PropertyNavElements';
 
-export default function PropertyNav() {
+
+export default function PropertyNav({step, navStep}) {
+	
+
+	const changeStep = (event) => {
+		const stepNav = event.currentTarget.id;
+		console.log(stepNav);
+		if(stepNav === "basic_info"){
+			navStep(1);
+		}
+		else if(stepNav === "property_detail"){
+			navStep(2);
+		}
+		else if(stepNav === "general_info"){
+			navStep(3);
+		}
+		else if(stepNav === "location_info"){
+			navStep(4);
+		}
+
+	}
 return (
 	<>
-    {/* <ul>
-        <li><NavLink class="active" to="/basic_info" className="link">Home</NavLink></li>
-        <li><NavLink to="/property_detail" >News</NavLink></li>
-        <li><a href="#contact" className="link">Contact</a></li>
-        <li><a href="#about" className="link">About</a></li>
-    </ul> */}
+   
 
 	<Nav>
 		<Bars />
 
 		<NavMenu>
-		<NavLink to='/basic_info'>
-			<NavSpan to='/basic_info'>1</NavSpan>Basic Info
+		<NavLink id="basic_info"  className={`${ step === 1 ? "active" : "" }`} onClick={changeStep}>
+			<NavSpan  className={`${ step === 1 ? "active" : "" }`}>1</NavSpan>Basic Info
 		</NavLink>
-		<NavLink to='/property_detail'>
-            <NavSpan to='/property_detail'>2</NavSpan>Property Detail
+		<NavLink id="property_detail"  className={`${ step === 2 ? "active" : undefined }`} onClick={changeStep}>
+            <NavSpan  className={`${ step === 2 ? "active" : undefined }`}>2</NavSpan>Property Detail
 		</NavLink>
-		<NavLink to='/general_info'>
-            <NavSpan to='/general_info'>3</NavSpan>General Info
+		<NavLink id="general_info"  className={`${ step === 3 ? "active" : undefined }`} onClick={changeStep}>
+            <NavSpan  className={`${ step === 3 ? "active" : undefined }`}>3</NavSpan>General Info
 		</NavLink>
-		<NavLink to='/location_info'>
-            <NavSpan to='/location_info'>4</NavSpan>Location Info
+		<NavLink id="location_info"   className={`${ step === 4 ? "active" : undefined }`} onClick={changeStep}>
+            <NavSpan  className={`${ step === 4 ? "active" : undefined }`}>4</NavSpan>Location Info
 		</NavLink>
 		</NavMenu>
 	</Nav>
+
+
 	</>
 );
 };
