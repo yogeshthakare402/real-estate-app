8961ec4774a0e444aaa2a2c038dfd2d34ef2f61f
Vidhi Dhama <dhamavidhi12@gmail.com>
2022-11-25 22:59:41 +0530

    general info form updated

--- real-estate/client/src/components/addNewProperty/GeneralInfo.js
+++ real-estate/client/src/components/addNewProperty/GeneralInfo.js
@@ -1,31 +1,24 @@
 import React, { useState } from 'react';
 import '../RealEstate.css'
-import {Link} from "react-router-dom";
-import location_info from './LocationInfo';
-import property_detail from './PropertyDetail';
-import PropertyNav from './PropertyNav';
 
-export default function GeneralInfo() {
-  const [inputs, setInputs] = useState({});
+export default function GeneralInfo({ nextStep, handleFormData, prevStep, values }) {
 
-  const handleChange = (event) => {
-    
-  }
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    
-  }
+const submitFormData = (e) => {
+    e.preventDefault();
+
+      nextStep();
+  
+  };
 
   return (
   <>
-    <PropertyNav/>
     <div className='card'>
-    <form method='POST' action='#' onSubmit={handleSubmit}>
+    <form method='POST' action='#'  onSubmit={submitFormData}>
         <section className='formSection'>
         <div className='formInput'>
-            <label>Name</label>
-            <select value={inputs} onChange={handleChange}>
+            <label>Owner</label>
+            <select name='owner'>
                 <option value="#">Owner</option>
             </select>
         </div>
@@ -35,47 +28,51 @@ export default function GeneralInfo() {
                 type="text" 
                 name="mobile" 
                 placeholder='Enter Mobile Number'
-                value={inputs.mobile || ""} 
-                onChange={handleChange}
+                value={values.mobile || ""} 
+                onChange={handleFormData("mobile")}
             />
         </div>
         <div className='formInput'>
             <label>Posted By</label>
-            <select value={inputs} onChange={handleChange}>
+            <select name='postedBy'>
                 <option value="#">Posted By</option>
             </select>
         </div>
         <div className='formInput'>
             <label>Sale Type</label>
-            <select value={inputs} onChange={handleChange}>
+            <select name='sale'>
                 <option value="#">Please Select</option>
             </select>
         </div>
         <div className='formInput'>
             <label>Featured Package</label>
-            <select value={inputs} onChange={handleChange}>
+            <select name='featuredPackage'>
                 <option value="#">Please Select</option>
             </select>
         </div>
         <div className='formInput'>
             <label>PPD Package</label>
-            <select value={inputs} onChange={handleChange}>
+            <select name='ppdPackage'>
                 <option value="#">Please Select</option>
         </select>
         </div>
         <div className='formInput'>
             <span className='eclipseCamera'>
-                {/* <img src={require("/images/eclipse.png")} alt="select folder"/> */}
+                
                 <img src='/images/camera.png' alt='camera' className='camera'/>
             </span>
             <span className='cameraText'>Add Photo</span>
         </div>
         <br/>
         <div className='formInput'>
-            <Link style={{textDecoration: 'none'}} to={"/property_detail"}><button className='cancelBtn'>Cancel</button></Link>
+            
+                <button className='cancelBtn' onClick={prevStep}>Cancel</button>
+                
         </div>
         <div className='formInput'>
-            <Link style={{textDecoration: 'none'}} to={"/location_info"}><button className='saveBtn'>Save & Continue</button></Link>
+            
+                <button className='saveBtn' type='submit'>Save & Continue</button>
+                
         </div>
         </section>
         
