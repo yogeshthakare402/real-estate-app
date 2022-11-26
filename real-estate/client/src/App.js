ffd5c8a51f8cf87ceaf5d5fb58ecf5258537914c
Vidhi Dhama <dhamavidhi12@gmail.com>
2022-11-26 13:08:51 +0530

    route updated

--- real-estate/client/src/App.js
+++ real-estate/client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import './App.css';
+import '../src/components/RealEstate.css';
 import Signin from './components/Signin';
 import Signup from './components/Signup';
 import PropertyListingPage from './components/PropertyListingPage';
@@ -12,11 +13,10 @@ function App() {
     <div className="App">
       <BrowserRouter>
       <Routes>
-      
-      <Route path='/' element={<Signin/>}/>
-      <Route path='/signup' element={<Signup/>}/>
-      <Route path='/propertyListingPage' element={<PropertyListingPage/>}/>
-      <Route path="/form" element={<Form/>}></Route>
+        <Route path='/' element={<Signin/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/propertyListingPage' element={<PropertyListingPage/>}/>
+        <Route path="/form" element={<Form/>}></Route>
       </Routes>
       </BrowserRouter>
     </div>
