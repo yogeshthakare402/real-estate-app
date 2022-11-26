19a2c80eeb8614df5183bd9be5a135c52fdeed46
Vidhi Dhama <dhamavidhi12@gmail.com>
2022-11-26 11:50:50 +0530

    form updated

--- real-estate/client/src/components/Form.js
+++ real-estate/client/src/components/Form.js
@@ -1,17 +1,25 @@
-
+// import "./App.css";
+// import { Container, Row, Col } from "react-bootstrap";
 import { useState } from "react";
-import BasicInfo from "./addNewProperty/BasicInfo";
-import PropertyDetail from "./addNewProperty/PropertyDetail";
-import GeneralInfo from "./addNewProperty/GeneralInfo";
-import LocationInfo from "./addNewProperty/LocationInfo";
-import Hello from "./Hello";{/***view page */}
-import PropertyNav from "./addNewProperty/PropertyNav";
-
+import BasicInfo from "./AddNewProperty/BasicInfo";
+import PropertyDetail from "./AddNewProperty/PropertyDetails";
+import GeneralInfo from "./AddNewProperty/GeneralInfo";
+import LocationInfo from "./AddNewProperty/LocationInfo";
+import PropertyListingPage from "./PropertyListingPage";
+import PropertyNav from "./AddNewProperty/PropertyNav";
+import CommonPage from './CommonPage';
 
 function Form() {
+
   const [step, setstep] = useState(1);
 
-  const [formData, setFormData] = useState({})
+  const [formData, setFormData] = useState({
+    "property" : "",
+    "length" : "",
+    "breadth" : "",
+    "area" : "",
+    "mobile" : ""
+  })
 
   const nextStep = () => {
     setstep(step + 1);
@@ -36,71 +44,78 @@ function Form() {
   }));
   }
 
-
   switch (step) {
        case 1:
       return (
-      <>
+        <>
+        <CommonPage>
         <PropertyNav step={step} navStep={navStep}/>
         <div className="form-elements">
           
                 <BasicInfo nextStep={nextStep} handleFormData={handleInputData} values={formData} />
               
         </div>
+        </CommonPage>
         </>
       );
         case 2:
       return (
         <>
+        <CommonPage>
         <PropertyNav step={step} navStep={navStep}/>
         <div className="form-elements">
           
-                <PropertyDetail nextStep={nextStep} prevStep={prevStep} handleFormData={handleInputData} values={formData} index={step} />
+                <PropertyDetail nextStep={nextStep} prevStep={prevStep} handleFormData={handleInputData} values={formData} />
               
         </div>
+        </CommonPage>
         </>
       );
          case 3:
         return (
-            <>
-        <PropertyNav step={step} navStep={navStep}/>
-          <div className="form-elements">
-            
-                  <GeneralInfo nextStep={nextStep} prevStep={prevStep} handleFormData={handleInputData} values={formData} index={step} />
-                
-          </div>
-          </>
+          <>
+          <CommonPage>
+          <PropertyNav step={step} navStep={navStep}/>
+            <div className="form-elements">
+              
+                    <GeneralInfo nextStep={nextStep} prevStep={prevStep} handleFormData={handleInputData} values={formData} />
+                  
+            </div>
+            </CommonPage>
+            </>
         );
            case 4:
         return (
-            <>
-        <PropertyNav step={step} navStep={navStep}/>
-          <div className="form-elements">
-            
-                  <LocationInfo nextStep={nextStep} prevStep={prevStep} handleFormData={handleInputData} values={formData} index={step} />
-                
-          </div>
-          </>
+          <>
+          <CommonPage>
+          <PropertyNav step={step} navStep={navStep}/>
+            <div className="form-elements">
+              
+                    <LocationInfo nextStep={nextStep} prevStep={prevStep} handleFormData={handleInputData} values={formData} />
+                  
+            </div>
+            </CommonPage>
+            </>
         );
         case 5:
-            
             return (
               <div className="form-elements">
                 
-                      <Hello values={formData} />{/**** view page  */}
+                      <PropertyListingPage values={formData} />
                     
               </div>
             );
         
-        default:
-      return (
-        <div className="form-elements">
-            {/*  add view page */}
-            <Hello/>
-        </div>
-      );
-  }
+        default: 
+        return (
+          <div className="form-elements">
+            
+                  <PropertyListingPage values={formData} />
+                
+          </div>
+        );
   
+  }
 }
 
 export default Form;
