8ba08b67e01b223db2c92d64b9c62e7c45b71976
Vidhi Dhama <dhamavidhi12@gmail.com>
2022-11-26 11:49:05 +0530

    sign in updated

---
+++ real-estate/client/src/components/Signin.js
@@ -0,0 +1,88 @@
+// import React from "react";
+import './RealEstate.css';
+import React, {useState } from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+function Signin() {
+  const [details, setDetails] = useState({
+    userid: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+  async function onsubmits(e) {
+    e.preventDefault(e);
+    const { userid, password } = details;
+
+  //   await fetch("http://localhost:8000/api/users/login",{
+  //     method : 'POST',
+  //     headers : {
+  //         'Accept': 'application/json',
+  //         'Content-Type': 'application/json'
+  //     },
+  //     body : JSON.stringify({
+  //       email, password
+  //     })
+  // }).then(navigate("/propertyListingPage"));
+
+    axios
+      .post("http://localhost:8000/api/users/login", {
+        email: userid,
+        password: password,
+      })
+      .then((res) => navigate("/propertyListingPage"))
+      .catch((err) => alert("Enter valid crendentials"));
+    console.log(details);
+    // PropertyListingPage();
+  }
+
+
+  return (
+    <form onSubmit={(e) => onsubmits(e)}>
+    <div className="container">
+      <div className="login">
+        <img
+          id="logo"
+          className="login-items"
+          alt="logo"
+          src="/images/logo.png"
+        />
+        <span id="credential-signin" className="login-items">
+          Enter your credentials to access your account
+        </span>
+        <input
+          type="text"
+          id="userid"
+          value={details.userid}
+          onChange={(e) => setDetails({ ...details, userid: e.target.value })}
+          className="login-items"
+          placeholder="         Email ID"
+        />
+        <input
+          type="password"
+          value={details.password}
+          onChange={(e) => setDetails({ ...details, password: e.target.value })}
+          className="login-items"
+          placeholder="         Password"
+        />
+        <button id="signin" className="login-items" onClick={(e)=>{onsubmits(e)}}>
+          Sign In
+        </button>
+        <Link to="/signup" id="link">
+          <span id="signup" className="login-items">
+            Sign Up
+          </span>
+        </Link>
+      </div>
+
+      <p className="extra-info">
+        Don’t have an account? <span id="extra-span">Sign up</span>
+      </p>
+    </div>
+    </form>
+  );
+}
+
+export default Signin;
