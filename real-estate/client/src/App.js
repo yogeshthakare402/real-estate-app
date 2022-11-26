b5b8dce6cce0ae3c5f7f3ccdd2d71913ba726adc
yogeshthakare402 <yogeshthakare402@gmail.com>
2022-11-27 00:34:26 +0530

    protected

--- real-estate/client/src/App.js
+++ real-estate/client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import './App.css';
+import Protected from './components/Protected';
 import Signin from './components/Signin';
 import Signup from './components/Signup';
 import PropertyListingPage from './components/PropertyListingPage';
@@ -8,6 +9,7 @@ import BasicInfo from './components/AddNewProperty/BasicInfo';
 import PropertyDetail from './components/AddNewProperty/PropertyDetails';
 import GeneralInfo from './components/AddNewProperty/GeneralInfo';
 import LocationInfo from './components/AddNewProperty/LocationInfo';
+import Form from './components/AddNewProperty/Form';
 
 
 function App() {
@@ -15,13 +17,19 @@ function App() {
     <div className="App">
       <BrowserRouter>
       <Routes>
-      <Route path='/signin' element={<Signin/>}/>
+      
+      <Route path='/' element={<Signin/>}/>
       <Route path='/signup' element={<Signup/>}/>
-      <Route path='/propertyListingPage' element={<PropertyListingPage/>}/>
-      <Route path='/basic_info' element={<BasicInfo/>}/>
-      <Route path='/property_detail' element={<PropertyDetail/>}/>
-      <Route path='/general_info' element={<GeneralInfo/>}/>
-      <Route path='/location_info' element={<LocationInfo/>}/>
+      <Route path='/propertyListingPage' element={
+         <Protected>
+          <PropertyListingPage/>
+         </Protected>
+      }/>
+      <Route path="/form" element={
+        <Protected>
+          <Form/>
+        </Protected>
+      }></Route>
       </Routes>
       </BrowserRouter>
     </div>
@@ -29,3 +37,25 @@ function App() {
 }
 
 export default App;
+
+
+{/* <Routes>
+        <Route path="/" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route
+          path="/property"
+          element={
+            <Protected>
+              <PropertyListingPage />
+            </Protected>
+          }
+        />
+        <Route
+          path="/property"
+          element={
+            <Protected>
+              <PropertyListingPage />
+            </Protected>
+          }
+        />
+      </Routes> */}
