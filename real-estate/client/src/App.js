87dbce116394bf988b6fee4d914a8de08e8423be
Vidhi Dhama <dhamavidhi12@gmail.com>
2022-11-26 11:41:10 +0530

    app file updated

--- real-estate/client/src/App.js
+++ real-estate/client/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import './App.css';
-import {BrowserRouter,Routes, Route} from 'react-router-dom'
-import BasicInfo from './components/addNewProperty/BasicInfo';
-import PropertyDetail from './components/addNewProperty/PropertyDetail';
-import GeneralInfo from './components/addNewProperty/GeneralInfo';
-import LocationInfo from './components/addNewProperty/LocationInfo';
+import Signin from './components/Signin';
+import Signup from './components/Signup';
+import PropertyListingPage from './components/PropertyListingPage';
+import Form from './components/Form';
 
 
 function App() {
@@ -12,10 +12,11 @@ function App() {
     <div className="App">
       <BrowserRouter>
       <Routes>
-        <Route path="basic_info" element={<BasicInfo />} />
-        <Route path="property_detail" element={<PropertyDetail />} />
-        <Route path="general_info" element={<GeneralInfo />} />
-        <Route path="location_info" element={<LocationInfo />} />
+      
+      <Route path='/' element={<Signin/>}/>
+      <Route path='/signup' element={<Signup/>}/>
+      <Route path='/propertyListingPage' element={<PropertyListingPage/>}/>
+      <Route path="/form" element={<Form/>}></Route>
       </Routes>
       </BrowserRouter>
     </div>
