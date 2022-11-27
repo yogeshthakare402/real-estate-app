0e8620ea567806e90ba1ea6cb5752f8285d9cad6
Vidhi Dhama <dhamavidhi12@gmail.com>
2022-11-27 22:36:09 +0530

    image are added

--- real-estate/client/src/components/CommonPage.js
+++ real-estate/client/src/components/CommonPage.js
@@ -5,21 +5,37 @@ function CommonPage({children}) {
 
     return (
         <section className='property'>
+           
             <aside className='asideLeft'>
                 <div><img className='logo' src='/images/logo.png' alt='Logo' /></div>
                 <ul className='data'>
-                    <li><b>Property</b></li>
-                    <li>Assistance</li>
-                    <li>Received Interest</li>
-                    <li>Send Interest</li>
-                    <li>Property Views</li>
-                    <li>Tariff Plan</li>
+                    <li>
+                        <img src='/images/property.png'  alt='property'/>
+                        <b>Property</b></li>
+                    <li>
+                    <img src='/images/assistance.png'  alt='assistance'/>
+                        Assistance</li>
+                    <li>
+                    <img src='/images/received_interest.png'  alt='received_interest'/>
+                        Received Interest</li>
+                    <li>
+                    <img src='/images/sent_interest.png'  alt='sent_interest'/>
+                        Send Interest</li>
+                    <li>
+                    <img src='/images/property_view.png'  alt='property_view'/>
+                        Property Views</li>
+                    <li>
+                    <img src='/images/tariff_plan.png'  alt='tariff_plan'/>
+                        Tariff Plan</li>
                 </ul>
-            </aside>
-            <aside className='asideRight'>
+            </aside> 
+            <div className='head'></div>
+             <aside className='asideRight'>
                 <header className='header'>
-                    <div>USER ID :</div>
-                    <div>User Name</div>
+                    <div className='header-id'>USER ID :</div>
+                    <div className='header-username'>
+                    <img src='/images/username.png'  alt='username'/>
+                        <span>User Name</span></div>
                 </header>
                 <div className='commonPage'>
                     {children}
